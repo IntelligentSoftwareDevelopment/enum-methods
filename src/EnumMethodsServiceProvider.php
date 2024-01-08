b914ede7ff768e91ea381c38fa871ae5c6177071
ins0mniac1 <ins0mniac1van@gmail.com>
2024-01-08 18:13:14 +0200

    fix composer json and add provider

---
+++ src/EnumMethodsServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace IntelligentSoftwareDevelopment\EnumMethods;
+
+use Illuminate\Support\ServiceProvider;
+
+class EnumMethodsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
